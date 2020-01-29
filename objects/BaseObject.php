09e6757a3b1bcf3f416d13d18d6c1f71e66da44d
Steve Massey <smasssey@inspectionsupport.net>
2020-01-29 08:51:07 -0500

    renamed Object to BaseObject to comply with reserved keywords in php72 and up

---
+++ objects/BaseObject.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Class Object is the root of all compliant objects in the OAF system.  All
+     * objects within the framework structure therefore have the same base
+     * properties and functions.
+     * @package PegasusPHP
+     */
+    class BaseObject {
+        /**
+         * Default Constructor
+         */
+        function __construct() {}
+        /**
+         * Default Destructor
+         */
+        function __destruct() {}
+    };
+?>
